03ac355e8ab7f254bb9dca78f47c753cb5f8eba0
NAMsdrawkcab <namhoangluu2002@gmail.com>
2023-05-08 15:05:28 -0400

    Add project files.

---
+++ Theme Park Prices/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        lblHeading = New Label()
+        lblTag = New Label()
+        picThemePark = New PictureBox()
+        cboVisit = New ComboBox()
+        grpOption = New GroupBox()
+        radVIP = New RadioButton()
+        radUnlimited = New RadioButton()
+        radNormal = New RadioButton()
+        lblAmount = New Label()
+        txtAmount = New TextBox()
+        lblCost = New Label()
+        btnCalculate = New Button()
+        btnClear = New Button()
+        CType(picThemePark, ComponentModel.ISupportInitialize).BeginInit()
+        grpOption.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' lblHeading
+        ' 
+        lblHeading.AutoSize = True
+        lblHeading.Font = New Font("STXingkai", 28.1999989F, FontStyle.Bold, GraphicsUnit.Point)
+        lblHeading.Location = New Point(286, 9)
+        lblHeading.Name = "lblHeading"
+        lblHeading.Size = New Size(301, 50)
+        lblHeading.TabIndex = 0
+        lblHeading.Text = "Starlight Avenue"
+        ' 
+        ' lblTag
+        ' 
+        lblTag.AutoSize = True
+        lblTag.Font = New Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        lblTag.Location = New Point(310, 59)
+        lblTag.Name = "lblTag"
+        lblTag.Size = New Size(252, 24)
+        lblTag.TabIndex = 1
+        lblTag.Text = "Check our prices below:"
+        ' 
+        ' picThemePark
+        ' 
+        picThemePark.Image = CType(resources.GetObject("picThemePark.Image"), Image)
+        picThemePark.Location = New Point(0, 101)
+        picThemePark.Name = "picThemePark"
+        picThemePark.Size = New Size(415, 293)
+        picThemePark.SizeMode = PictureBoxSizeMode.StretchImage
+        picThemePark.TabIndex = 2
+        picThemePark.TabStop = False
+        ' 
+        ' cboVisit
+        ' 
+        cboVisit.Font = New Font("Tahoma", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        cboVisit.FormattingEnabled = True
+        cboVisit.Items.AddRange(New Object() {"Day Visit", "Hotel Visit"})
+        cboVisit.Location = New Point(421, 102)
+        cboVisit.Name = "cboVisit"
+        cboVisit.Size = New Size(440, 29)
+        cboVisit.TabIndex = 3
+        cboVisit.Text = "Choose Your Visit"
+        ' 
+        ' grpOption
+        ' 
+        grpOption.BackColor = Color.Cornsilk
+        grpOption.Controls.Add(radVIP)
+        grpOption.Controls.Add(radUnlimited)
+        grpOption.Controls.Add(radNormal)
+        grpOption.Font = New Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point)
+        grpOption.Location = New Point(421, 150)
+        grpOption.Name = "grpOption"
+        grpOption.Size = New Size(440, 127)
+        grpOption.TabIndex = 4
+        grpOption.TabStop = False
+        grpOption.Text = "Choose Your Experience"
+        ' 
+        ' radVIP
+        ' 
+        radVIP.AutoSize = True
+        radVIP.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        radVIP.Location = New Point(6, 88)
+        radVIP.Name = "radVIP"
+        radVIP.Size = New Size(264, 22)
+        radVIP.TabIndex = 2
+        radVIP.TabStop = True
+        radVIP.Text = "VIP: Unlimited rides and skip the line"
+        radVIP.UseVisualStyleBackColor = True
+        ' 
+        ' radUnlimited
+        ' 
+        radUnlimited.AutoSize = True
+        radUnlimited.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        radUnlimited.Location = New Point(6, 57)
+        radUnlimited.Name = "radUnlimited"
+        radUnlimited.Size = New Size(187, 22)
+        radUnlimited.TabIndex = 1
+        radUnlimited.TabStop = True
+        radUnlimited.Text = "Upgrade: Unlimited rides"
+        radUnlimited.UseVisualStyleBackColor = True
+        ' 
+        ' radNormal
+        ' 
+        radNormal.AutoSize = True
+        radNormal.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        radNormal.Location = New Point(6, 26)
+        radNormal.Name = "radNormal"
+        radNormal.Size = New Size(266, 22)
+        radNormal.TabIndex = 0
+        radNormal.TabStop = True
+        radNormal.Text = "Normal: 15 rides per person per day"
+        radNormal.UseVisualStyleBackColor = True
+        ' 
+        ' lblAmount
+        ' 
+        lblAmount.AutoSize = True
+        lblAmount.Font = New Font("Tahoma", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        lblAmount.Location = New Point(464, 301)
+        lblAmount.Name = "lblAmount"
+        lblAmount.Size = New Size(219, 21)
+        lblAmount.TabIndex = 5
+        lblAmount.Text = "How many or how long?"
+        ' 
+        ' txtAmount
+        ' 
+        txtAmount.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point)
+        txtAmount.Location = New Point(751, 295)
+        txtAmount.Name = "txtAmount"
+        txtAmount.Size = New Size(79, 31)
+        txtAmount.TabIndex = 6
+        txtAmount.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' lblCost
+        ' 
+        lblCost.Font = New Font("Showcard Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        lblCost.Location = New Point(12, 428)
+        lblCost.Name = "lblCost"
+        lblCost.Size = New Size(849, 58)
+        lblCost.TabIndex = 7
+        lblCost.Text = "The total cost of your visit is $XX.XX"
+        lblCost.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point)
+        btnCalculate.Location = New Point(464, 347)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(120, 40)
+        btnCalculate.TabIndex = 8
+        btnCalculate.Text = "Check Price"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point)
+        btnClear.Location = New Point(710, 347)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(120, 40)
+        btnClear.TabIndex = 9
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.PaleGoldenrod
+        ClientSize = New Size(873, 495)
+        Controls.Add(btnClear)
+        Controls.Add(btnCalculate)
+        Controls.Add(lblCost)
+        Controls.Add(txtAmount)
+        Controls.Add(lblAmount)
+        Controls.Add(grpOption)
+        Controls.Add(cboVisit)
+        Controls.Add(picThemePark)
+        Controls.Add(lblTag)
+        Controls.Add(lblHeading)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Starlight Ave Prices"
+        CType(picThemePark, ComponentModel.ISupportInitialize).EndInit()
+        grpOption.ResumeLayout(False)
+        grpOption.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblTag As Label
+    Friend WithEvents picThemePark As PictureBox
+    Friend WithEvents cboVisit As ComboBox
+    Friend WithEvents grpOption As GroupBox
+    Friend WithEvents radVIP As RadioButton
+    Friend WithEvents radUnlimited As RadioButton
+    Friend WithEvents radNormal As RadioButton
+    Friend WithEvents lblAmount As Label
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents lblCost As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ Theme Park Prices/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+    'Variables to be used in the program; _decPrice changes based on option
+    Private _decDayN As Decimal = 74.99D
+    Private _decDayU As Decimal = 89.99D
+    Private _decDayVIP As Decimal = 99.99D
+    Private _decHotelN As Decimal = 109.99D
+    Private _decHotelU As Decimal = 124.99D
+    Private _decHotelVIP As Decimal = 149.99D
+    Private _decPrice As Decimal
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ClearForm()
+    End Sub
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim decTotal As Decimal
+        Dim intValue As Integer
+        If ValidateInput() = True Then
+            intValue = Convert.ToInt32(txtAmount.Text)
+            If cboVisit.SelectedIndex = 0 Then
+                If radUnlimited.Checked = True Then
+                    _decPrice = _decDayU
+                ElseIf radVIP.Checked = True Then
+                    _decPrice = _decDayVIP
+                Else
+                    _decPrice = _decDayN
+                End If
+            Else
+                If radUnlimited.Checked = True Then
+                    _decPrice = _decHotelU
+                ElseIf radVIP.Checked = True Then
+                    _decPrice = _decHotelVIP
+                Else
+                    _decPrice = _decHotelN
+                End If
+            End If
+            decTotal = Calculate(intValue)
+            lblCost.Visible = True
+            lblCost.Text = "The total cost of your visit is " & decTotal.ToString("C")
+        End If
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ClearForm()
+    End Sub
+    Private Sub cboVisit_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboVisit.SelectedIndexChanged
+        If cboVisit.SelectedIndex = 0 Then
+            grpOption.Visible = True
+            radNormal.Text = "Normal: 15 rides per person (" & _decDayN.ToString("C") & " per person)"
+            radUnlimited.Text = "Upgrade: Unlimited rides (" & _decDayU.ToString("C") & " per person)"
+            radVIP.Text = "VIP: Unlimited rides and skip the line (" & _decDayVIP.ToString("C") & " per person)"
+            radNormal.Checked = True
+            radUnlimited.Checked = False
+            radVIP.Checked = False
+            lblAmount.Visible = True
+            lblAmount.Text = "How many people? (Max: 5)"
+            txtAmount.Visible = True
+            txtAmount.Clear()
+            txtAmount.Focus()
+            lblCost.Visible = False
+            btnCalculate.Visible = True
+            btnClear.Visible = True
+        End If
+        If cboVisit.SelectedIndex = 1 Then
+            grpOption.Visible = True
+            radNormal.Text = "Normal: 15 rides per person per day (" & _decHotelN.ToString("C") & " per night)"
+            radUnlimited.Text = "Upgrade: Unlimited rides (" & _decHotelU.ToString("C") & " per night)"
+            radVIP.Text = "VIP: Unlimited rides and skip the line (" & _decHotelVIP.ToString("C") & " per night)"
+            radNormal.Checked = True
+            radUnlimited.Checked = False
+            radVIP.Checked = False
+            lblAmount.Visible = True
+            lblAmount.Text = "How many nights? (Max: 5)"
+            txtAmount.Visible = True
+            txtAmount.Clear()
+            txtAmount.Focus()
+            lblCost.Visible = False
+            btnCalculate.Visible = True
+            btnClear.Visible = True
+        End If
+    End Sub
+    Private Sub ClearForm()
+        'Clears form; called when program loads and when the Clear button is clicked
+        cboVisit.SelectedIndex = -1
+        cboVisit.Text = "Choose Your Visit"
+        grpOption.Visible = False
+        lblAmount.Visible = False
+        lblCost.Visible = False
+        txtAmount.Clear()
+        txtAmount.Visible = False
+        btnCalculate.Visible = False
+        btnClear.Visible = False
+        radNormal.Checked = True
+        radUnlimited.Checked = False
+        radVIP.Checked = False
+    End Sub
+    Private Function Calculate(ByVal intNum As Integer) As Decimal
+        'Calculates cost of visit; takes integer from textbox as input
+        Dim decTicketCost As Decimal
+        decTicketCost = _decPrice * intNum
+        Return decTicketCost
+    End Function
+    Private Function ValidateInput() As Boolean
+        'Validates input; based on Weekly Assignmnet 7; utilizes exceptions
+        Dim intNumber As Integer
+        Dim blnValid As Boolean = False
+        Try
+            intNumber = Convert.ToInt32(txtAmount.Text)
+            If intNumber > 0D And intNumber <= 5D Then
+                blnValid = True
+            Else
+                MsgBox("Please enter a number greater than 0 and up to 5.", , "Error")
+                txtAmount.Focus()
+                txtAmount.Clear()
+            End If
+        Catch Exception As FormatException
+            MsgBox("Please enter a valid number.", , "Error")
+            txtAmount.Focus()
+            txtAmount.Clear()
+        Catch Exception As OverflowException
+            MsgBox("That number is too big!", , "Error")
+            txtAmount.Focus()
+            txtAmount.Clear()
+        Catch Exception As SystemException
+            MsgBox("Entry invalid. Please enter a valid number to be used for calculation.", , "Error")
+            txtAmount.Focus()
+            txtAmount.Clear()
+        End Try
+        Return blnValid
+    End Function
+End Class
